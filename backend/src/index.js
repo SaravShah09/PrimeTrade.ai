063f4f8d067ce7b9dfb8bd98ae33ad2d0e23f67f
Alone <141217880+SaravShah09@users.noreply.github.com>
2026-01-24 23:40:35 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://primetradeai-saravs-projects-00d49e14.vercel.app"
+    "https://primetrade-pi.vercel.app/"
   );
   res.header(
     "Access-Control-Allow-Headers",
